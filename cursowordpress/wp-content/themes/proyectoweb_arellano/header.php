d37466a15ccb2ac1fcc2f4c1ea1a693a9734ac46
Arellano1996 <90539222+Arellano1996@users.noreply.github.com>
2021-09-11 22:27:37 -0500

    Subida de documentos

---
+++ cursowordpress/wp-content/themes/proyectoweb_arellano/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!-- <html lang="es"> -->
+<html <?php language_attributes(); ?> >
+
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+
+	<title> <?php wp_title('  | ', true, 'right'); ?> <?php bloginfo('name'); ?> </title>
+	
+	<!--link <rel="stylesheet" href="style.css" > -->
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
+	
+	<div id="global-container">
+		
+		
+		<header id="main-header">
+			
+			<h1 class="site-title"><?php bloginfo('name'); ?></h1>
+			<h2 class="site-description"><?php bloginfo('description'); ?></h2>
+		
+		</header><!-- /#main-header -->
+		
+		
+		<nav id="main-nav">
+			<ul class="menu">
+				<li><a href="">Menú item 1</a></li>
+				<li><a href="">Menú item 2</a></li>
+				<li><a href="">Menú item 3</a></li>
+				<li><a href="">Menú item 4</a></li>
+				<li><a href="">Menú item 5</a></li>
+				<li><a href="">Menú item 6</a></li>
+			</ul>
+		</nav><!-- /#main-nav -->
+		
+		
+		<section id="global-content" class="cf">
