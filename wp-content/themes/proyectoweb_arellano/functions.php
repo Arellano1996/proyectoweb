06942b882d464337ec898d5a8e839a955685a7d2
Arellano1996 <90539222+Arellano1996@users.noreply.github.com>
2021-09-11 22:41:05 -0500

    h

---
+++ wp-content/themes/proyectoweb_arellano/functions.php
@@ -0,0 +1,9 @@
+<?php
+function load_styles(){
+
+    wp_register_style('theme_style', get_stylesheet_uri(), '','1.0', 'all');
+    wp_enqueue_style('theme_style');
+}
+
+add_action('wp_enqueue_scripts', 'load_styles');
+?>
